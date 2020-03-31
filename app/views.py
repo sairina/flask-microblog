9e498bcbbcc9b6ebece1508e8c7c8deb317d39a3
sairina <sairina.merinotsui@gmail.com>
2020-03-30 22:03:38 -0700

    added array of posts to populate in jinja

--- app/views.py
+++ app/views.py
@@ -1,8 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-
-
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in the neighborhood!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'That movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
